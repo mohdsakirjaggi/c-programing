ab7aa6bfc8835dca578f423ab58ec3374847a60b
Shakir <mohdsakirjaggi1@gmail.com>
2026-08-11 22:17:29 +0530

    Q-03

---
+++ .vscode/P-03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+    float l,b,rectangular;
+    printf("Enter lenth of rectangular :\n");
+    scanf("%f",&l);
+    printf("Enter breath of rectangular :\n");
+    scanf("%f",&b);
+    rectangular=l*b;
+    printf("Area of rectangular is %f",rectangular);
+    return 0;
+}
---
+++ .vscode/P-04.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    
+    float r,circle,circumfrence,PI=3.14;
+    printf("Enter redius :\n");
+    scanf("%d",&circle);
+    circle= PI*r*r;
+    circumfrence=2*PI*r;
+    printf("Area of circle is %d \n " ,circle );
+    
+}
+
+
+
