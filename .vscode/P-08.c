d3078a5aa2bea1ae922cd5ea896e94c8de0c89c8
Shakir <mohdsakirjaggi1@gmail.com>
2026-08-13 21:59:42 +0530

    Q-08

--- .vscode/P-08.c
+++ .vscode/P-08.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(){
-    float s ;
+    float s ;    //s=sum of natural number
     int n;
     printf("Enter the value of n ");
     scanf("%d",&n);
     s=n*(n+1)/2;
-    printf("Sum of natural number is %.2f",s);
+    printf("Sum of natural numbers is %.2f",s);
 }
