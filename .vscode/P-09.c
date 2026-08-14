162006174fc561f1f5c43735b0ed83f968bad6c3
Shakir <mohdsakirjaggi1@gmail.com>
2026-08-14 21:51:43 +0530

    Q-09

---
+++ .vscode/P-09.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    float p,r,t,si,a,ci;
+    printf("Enter principal ,rate and time:\n");
+    scanf("%f %f %f",&p,&r,&t);
+    //Simple interst
+    si=p*r*t/100;
+    //Compound interest
+    a=p*((1+r/100),t);
+    ci=a-p;
+    printf("Simple interest = %.2f\n Compound interest = %f",si,ci);
+    
+
+    return 0;
+   
+}
