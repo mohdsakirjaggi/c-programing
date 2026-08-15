21662f59f1e3eabbbaa7fe7c88d48be53ddbe3a4
Shakir <mohdsakirjaggi1@gmail.com>
2026-08-15 15:38:26 +0530

    Q-12

---
+++ .vscode/p-12.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter an integer:\n");
+    scanf("%d",&n);
+    if(n >= 0 ) {
+        if(n == 0)
+        printf("The number is Zero");
+        else
+        printf("The number is Positive");
+
+    }
+    else {
+        printf("The number is Negative");
+    }
+    return 0;
+}
