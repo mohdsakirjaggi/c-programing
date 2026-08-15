d8b5231dad170f38220d689d8107710055f473bf
Shakir <mohdsakirjaggi1@gmail.com>
2026-08-15 15:31:50 +0530

    Q-11

---
+++ .vscode/P-11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter a integer:\n");
+    scanf("%d",&n);
+    if (n % 2 ==0)
+        printf("The number is Even");
+        else
+        printf("The number is Odd");
+        return 0;
+}
