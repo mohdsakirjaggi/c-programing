855708a1c47062065fbc1002d85394fd461de2f1
Shakir <mohdsakirjaggi1@gmail.com>
2026-08-21 22:46:24 +0530

    Q-24

--- .vscode/P-24.c
+++ .vscode/P-24.c
@@ -6,5 +6,30 @@ Above at ₹12/unit*/
 #include <stdio.h>
 int main()
 {
-    int
+    int units;
+    float bill;
+    printf("Enter the numbers of units consumed\n");
+    scanf("%d", &units);
+    if (units <= 100)
+    {
+        bill = units * 5;
+        printf("Electricity bill=Rs.%.2f\n", bill);
+    }
+    else if (units <= 200)
+    {
+        bill = (100 * 5) + (units - 100) * 7;
+        printf("Electricity bill=Rs.%f.2\n", bill);
+    }
+    else if (units <= 300)
+    {
+        bill = (100 * 5) + (100 * 7) + (units - 200) * 10;
+        printf("Electricity bill=Rs.%f.2\n", bill);
+    }
+    else
+    {
+        bill = (100 * 5) + (100 * 7) + (100 * 10) + (units - 300) * 12;
+        printf("Electricity bill=Rs.%f.2\n", bill);
+    }
+    printf("Thank you!");
+    return 0;
 }
