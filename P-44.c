4d863b111a10754042ddf0d567de2cd256c834e4
Shakir <mohdsakirjaggi1@gmail.com>
2026-08-31 22:01:08 +0530

    Q-44

---
+++ P-44.c
@@ -0,0 +1,21 @@
+// Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    float sum = 1.0;
+
+    printf("Enter number of terms:\n ");
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i++)
+    {
+        sum = sum + (float)(2 * i - 1) / (2 * i);
+    }
+
+    printf("Sum = %.4f", sum);
+
+    return 0;
+}
