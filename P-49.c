ff13ace58d1feb6eaaeffb9d29ff782ac59ee541
Shakir <mohdsakirjaggi1@gmail.com>
2026-09-03 21:22:06 +0530

    Q-49

---
+++ P-49.c
@@ -0,0 +1,26 @@
+// Write a program to print the following pattern:
+/*5
+45
+345
+2345
+12345*/
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for (i = n; i >= 1; i--)
+    {
+        for (j = i; j <= n; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
