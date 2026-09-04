59ea714e8c50c80301a14dcde6e2477c02dae90a
Shakir <mohdsakirjaggi1@gmail.com>
2026-09-04 22:45:07 +0530

    Q-51

--- .vscode/P-2.c
+++ .vscode/P-2.c
@@ -31,7 +31,7 @@ int main()
     return 0;
 }*/
 
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main()
 {
@@ -62,6 +62,25 @@ int main()
     else
     {
         printf("No Profit, No Loss\n");
+    }
+
+    return 0;
+}*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= 5; j++)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
     }
 
     return 0;
